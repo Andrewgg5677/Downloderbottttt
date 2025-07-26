feddf0a1a69ce357c9d7d15c4e230e984876b8a4
Andrewgg5677 <bilalstorein@gmail.com>
2025-07-26 21:42:40 +0300

    Update My_bot.py

--- My_bot.py
+++ My_bot.py
@@ -1,10 +1,12 @@
 import telebot
+import os
 
-bot = telebot.TeleBot("")
 
+TOKEN = os.environ.get("TOKEN")
+bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "MBM, how are you doing?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
